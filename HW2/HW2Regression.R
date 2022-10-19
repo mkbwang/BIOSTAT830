08b8872d9e3f82d289bd01abdc9c46f13420ddd2
Mukai Wang <wangmk@umich.edu>
2022-10-18 23:13:14 -0400

    Refit regressions for problem 3 and 4.

--- HW2/HW2Regression.R
+++ HW2/HW2Regression.R
@@ -4,95 +4,62 @@ library(reshape2)
 library(cowplot)
 
 rm(list=ls())
-michigan_deaths <- read.csv(file.path('HW2', 'michigan_deaths_by_week.csv'))
+michigan_deaths <- read.csv(file.path('HW2', 'weekly_deaths_by_age.csv'))
 
-
-adult_deaths <- michigan_deaths %>%
-  filter(Sex == "All Sex" & Age.Group %in% c("15-24 Years", "25-34 Years", "35-44 Years", "45-54 Years", "55-64 Years")) %>%
-  group_by(End.Week) %>% summarise(death_counts = sum(covid))
-adult_deaths$age_group <- 0# adults
-adult_deaths$population <- 10050811*(1-0.214-0.181)
-
-
-
-senior_deaths <- michigan_deaths %>%
-  filter(Sex == "All Sex" & Age.Group %in% c("65-74 Years", "75-84 Years", "85 Years and Over")) %>%
-  group_by(End.Week) %>% summarise(death_counts = sum(covid))
-senior_deaths$age_group <- 1#senior
-senior_deaths$population <- 10050811*0.181
-
-deaths_by_age_group <- rbind(adult_deaths, senior_deaths)
-deaths_by_age_group$End.Week <- as.Date(deaths_by_age_group$End.Week, format="%Y-%m-%d")
+michigan_deaths$End.Week <- as.Date(michigan_deaths$End.Week, format="%Y-%m-%d")
 
 
 knot1 <- as.Date("2020-12-14", format="%Y-%m-%d")
 knot2 <- as.Date("2021-06-26", format="%Y-%m-%d")
 knot3 <- as.Date("2021-11-29", format="%Y-%m-%d")
 knot4 <- as.Date("2022-05-22", format="%Y-%m-%d")
-vaccine_available <- deaths_by_age_group$End.Week > knot1
-delta <- deaths_by_age_group$End.Week > knot2
-omicron1 <- deaths_by_age_group$End.Week > knot3
-omicron2 <- deaths_by_age_group$End.Week > knot4
+vaccine_available <- michigan_deaths$End.Week > knot1
+delta <- michigan_deaths$End.Week > knot2
+omicron1 <- michigan_deaths$End.Week > knot3
+omicron2 <- michigan_deaths$End.Week > knot4
 
-deaths_by_age_group$vaccination <- as.numeric(vaccine_available)
-deaths_by_age_group$delta <- as.numeric(delta)
-deaths_by_age_group$omicron1 <- as.numeric(omicron1)
-deaths_by_age_group$omicron2 <- as.numeric(omicron2)
+michigan_deaths$vaccination <- as.numeric(vaccine_available)
+michigan_deaths$delta <- as.numeric(delta)
+michigan_deaths$omicron1 <- as.numeric(omicron1)
+michigan_deaths$omicron2 <- as.numeric(omicron2)
 
 # deaths_by_age_group$End.Week <- as.numeric(as.POSIXct(deaths_by_age_group$End.Week))
-deaths_by_age_group <- deaths_by_age_group[order(deaths_by_age_group$End.Week,
-                                                 deaths_by_age_group$age_group),]
+michigan_deaths <- michigan_deaths[order(michigan_deaths$End.Week,
+                                         michigan_deaths$age_group),]
+michigan_deaths$population <- rep(c(2.14e6, 6.05e6, 1.81e6), 132)
 
 
 # load vaccine csv file
-michigan_vaccine <- read.csv(file.path('HW2', 'michigan_vaccines.csv'))
-michigan_vaccine$Date <- as.Date(michigan_vaccine$Date, format="%m/%d/%Y")
+michigan_vaccine <- read.csv(file.path('HW2', 'complete_vaccination.csv'))
+michigan_vaccine$Date <- as.Date(michigan_vaccine$Date, format="%Y-%m-%d")
 michigan_vaccine <- michigan_vaccine[order(michigan_vaccine$Date), ]
-# process the full vaccination percentage for 18-64 age group
-michigan_vaccine_percent <- michigan_vaccine %>% mutate(Pop5PlusA = Administered_Dose1_Recip_5Plus / Administered_Dose1_Recip_5PlusPop_Pct,
-                                            Pop5PlusS = Series_Complete_5Plus / Series_Complete_5PlusPop_Pct,
-                                            Pop18PlusA = Administered_Dose1_Recip_18Plus / Administered_Dose1_Recip_18PlusPop_Pct,
-                                            Pop18PlusS = Series_Complete_18Plus / Series_Complete_18PlusPop_Pct,
-                                            Pop65PlusA = Administered_Dose1_Recip_65Plus / Administered_Dose1_Recip_65PlusPop_Pct,
-                                            Pop65PlusS = Series_Complete_65Plus / Series_Complete_65PlusPop_Pct) %>%
-  mutate(Administered_Dose1_Recip_0to17_Pct =
-           (Administered_Dose1_Recip_5Plus - Administered_Dose1_Recip_18Plus) /
-           (Pop5PlusA - Pop18PlusA),
-         Series_Complete_0to17_Pct = (Series_Complete_5Plus - Series_Complete_18Plus) /
-           (Pop5PlusS - Pop18PlusS),
-         Administered_Dose1_Recip_18to64_Pct =
-           (Administered_Dose1_Recip_18Plus - Administered_Dose1_Recip_65Plus) /
-           (Pop18PlusA - Pop65PlusA),
-         Series_Complete_18to64_Pct = (Series_Complete_18Plus - Series_Complete_65Plus) /
-           (Pop18PlusS - Pop65PlusS)) %>%
-  dplyr::select(Date, Series_Complete_0to17_Pct, Series_Complete_18to64_Pct, Series_Complete_65PlusPop_Pct)
-
-
-michigan_vaccine_percent[is.na(michigan_vaccine_percent)] <- 0
 
 # matching dates of the vaccine data and the death count data
-deaths_alldates <- deaths_by_age_group$End.Week - 3
-vaccines_alldates <- michigan_vaccine_percent$Date
+deaths_alldates <- michigan_deaths$End.Week - 3
+vaccines_alldates <- michigan_vaccine$Date
 dates_match <- vaccines_alldates[vaccines_alldates %in% deaths_alldates]
-vaccines_subset <- michigan_vaccine_percent[michigan_vaccine_percent$Date %in% dates_match,
-                                            c("Date", "Series_Complete_18to64_Pct", "Series_Complete_65PlusPop_Pct")]
-vaccines_subset_long <- rep(0, nrow(vaccines_subset)*2)
-vaccines_subset_long[seq(1, 183, 2)] <- vaccines_subset$Series_Complete_18to64_Pct
-vaccines_subset_long[seq(2, 184, 2)] <- vaccines_subset$Series_Complete_65PlusPop_Pct
+vaccines_subset <- michigan_vaccine[michigan_vaccine$Date %in% dates_match,]
+vaccines_subset_long <- rep(0, nrow(vaccines_subset)*3)
+vaccines_subset_long[seq(1, 274, 3)] <- vaccines_subset$Under18
+vaccines_subset_long[seq(2, 275, 3)] <- vaccines_subset$Between19_64
+vaccines_subset_long[seq(3, 276, 3)] <- vaccines_subset$Over65
 #vwhich(deaths_by_age_group$End.Week-3 == min(vaccines_subset$Date))
 deaths_by_age_group$fullvc <- 0
-deaths_by_age_group$fullvc[81:264] <- vaccines_subset_long
+start_pos <- min(which(michigan_deaths$End.Week-3 == min(dates_match)))
+end_pos <- max(which(michigan_deaths$End.Week-3 == max(dates_match)))
+michigan_deaths$fullvc <- 0
+michigan_deaths$fullvc[start_pos:end_pos] <- vaccines_subset_long
 
-deaths_by_age_group$End.Week <- rep(seq(0, 131), each=2)
+michigan_deaths$End.Week <- rep(seq(0, 131), each=3)
 
 # library(mgcv)
 
 poi_model <- glm(death_counts ~ offset(log(population)) + End.Week + age_group,
-                    family=poisson("log"), data=deaths_by_age_group)
+                    family=poisson("log"), data=michigan_deaths)
 
 library(MASS)
 nb_model <- glm.nb(death_counts ~ offset(log(population)) + End.Week + age_group,
-                   data=deaths_by_age_group)
+                   data=michigan_deaths)
 
 
 poi_knots_model <- glm(death_counts ~ offset(log(population)) + End.Week  +
@@ -100,26 +67,27 @@ poi_knots_model <- glm(death_counts ~ offset(log(population)) + End.Week  +
                          I(delta * End.Week) +
                          I(omicron1 * End.Week) +
                          I(omicron2 * End.Week) + age_group,
-                       family=poisson("log"), data=deaths_by_age_group)
+                       family=poisson("log"), data=michigan_deaths)
 
 
 nb_knots_model <- glm.nb(death_counts ~ offset(log(population)) + End.Week  +
                         I(vaccination * End.Week) +
                         I(delta * End.Week) +
                         I(omicron1 * End.Week) +
-                        I(omicron2 * End.Week) + age_group, data=deaths_by_age_group)
+                        I(omicron2 * End.Week) + age_group, data=michigan_deaths)
 
 
-nb_knots_vaccination_model <- glm.nb(death_counts ~ offset(log(population)) + End.Week  +
+nb_knots_vaccination_model <- glm.nb(death_counts ~ offset(log(population)) + End.Week +  age_group + fullvc+
                                        I(vaccination * End.Week) +
                                        I(delta * End.Week) +
                                        I(omicron1 * End.Week) +
-                                       I(omicron2 * End.Week) + age_group + fullvc, data=deaths_by_age_group)
+                                       I(omicron2 * End.Week) , data=michigan_deaths)
+
 
 nb_knots_vaccination_age_model <- glm.nb(death_counts ~ offset(log(population)) + End.Week  +
+                                           age_group + fullvc + I(age_group * fullvc)+
                                            I(vaccination * End.Week) +
                                            I(delta * End.Week) +
                                            I(omicron1 * End.Week) +
-                                           I(omicron2 * End.Week) + age_group + fullvc + I(age_group * fullvc), data=deaths_by_age_group)
-
+                                           I(omicron2 * End.Week) , data=michigan_deaths)
 
